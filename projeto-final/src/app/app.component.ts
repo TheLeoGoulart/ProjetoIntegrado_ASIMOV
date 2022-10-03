2475722f192e3d1a56dc079f481d8559436048a4
TheLeoGoulart <Leonardo.p.goulart4@gmail.com>
2022-10-03 14:19:52 -0300

    Ajustes iniciais

--- projeto-final/src/app/app.component.ts
+++ projeto-final/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  constructor(public authService: AuthService) {}
   title = 'Projeto final';
 }
