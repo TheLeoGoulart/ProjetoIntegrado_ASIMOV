fd296d572c5f570f70c2873b2010f6cd2595ea92
brunop6 <peresbruno6@gmail.com>
2022-10-04 15:30:49 -0300

    Modificações CSS

--- projeto-final/src/app/sign-in/sign-in.component.ts
+++ projeto-final/src/app/sign-in/sign-in.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from ".././shared";
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.css']
 })
 
 export class SignInComponent implements OnInit {
